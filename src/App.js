efd01534d51201e4d332500adcb1de175c5c17dc
masoudkarimif <karimif.masoud@gmail.com>
2023-11-22 22:36:15 -0700

    adds start state

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [desiredClass, setDesiredClass] = useState("");
   return (
     <div className="App">
-      <section className={desiredClass}>
+      <section>
         <img
           id="mru-logo"
           src="https://res.cloudinary.com/mkf/image/upload/v1686003313/mru-white_learui.png"
         />
       </section>
-      <input
-        type="text"
-        placeholder="Write Class Name Here"
-        value={desiredClass}
-        onChange={(e) => setDesiredClass(e.target.value)}
-      />
+      <input type="text" placeholder="Write Class Name Here" />
     </div>
   );
 }
